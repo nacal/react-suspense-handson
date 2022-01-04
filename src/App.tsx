87199f93da86e0965f9c3a7abd819e41f89781e2
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2022-01-04 17:07:01 +0900

    Suspence rerender

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import "./App.css";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const AlwaysSuspend: React.VFC = () => {
-  throw sleep(1000);
+export const SometimesSuspend: React.VFC = () => {
+  if (Math.random() < 0.5) {
+    throw sleep(1000);
+  }
+  return <p>Hello, world!</p>;
 };
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
       <Suspense fallback={<p>Loading...</p>}>
-        <AlwaysSuspend />
+        <SometimesSuspend />
+        <button className="border p-1" onClick={() => setCount((c) => c + 1)}>
+          {count}
+        </button>
       </Suspense>
     </div>
   );
